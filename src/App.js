a3d742538a94bcf932a0b417a2e83dcf1915b305
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-07-28 22:14:33 +0600

    Chapter 10: Thinking in React Components, Composition, and Reusability >> Tutorial 9: Using Composition to Make a Reusable Box

--- src/App.js
+++ src/App.js
@@ -91,17 +91,14 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && children}
+      {isOpen && children}
     </div>
   );
 }
@@ -129,7 +126,7 @@ function Movie({ movie }) {
     </li>
   );
 }
-function WatchedBox() {
+/*function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData);
 
   const [isOpen2, setIsOpen2] = useState(true);
@@ -151,7 +148,7 @@ function WatchedBox() {
     </div>
   );
 }
-
+*/
 function WatchedMoviesList({ watched }) {
   return (
     <ul className="list">
@@ -214,6 +211,7 @@ function WatchedSummary({ watched }) {
 }
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -222,10 +220,13 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />{" "}
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );

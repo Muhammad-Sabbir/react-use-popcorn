81f63473a62ec8c6fd339acc01a47a0a242639b3
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-07-31 18:48:35 +0600

    Chapter 10: Thinking in React Components, Composition, and Reusability >> Tutorial 15: Improving Reusability With Props

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import StartRating from "./StarRating";
+import { useState } from "react";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StartRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p> This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5} />
-    <StartRating maxRating={10} />
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StartRating
+      maxRating={10}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
     <StartRating />
+    <Test />
   </React.StrictMode>
 );

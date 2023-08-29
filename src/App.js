662b54b566af9408abf050b5838d05e15160ed04
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-08-29 19:24:51 +0600

    Chapter 13: Custom Hooks, Refs, and More State >> 5. Initializing State With a Callback (Lazy Initial State)

--- src/App.js
+++ src/App.js
@@ -179,11 +179,17 @@ const KEY = "5922d93e";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  // const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    //we cant pass parameter into this function.
 
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
   function handleSelectedMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -192,11 +198,17 @@ export default function App() {
   }
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+    // localStorage.setItem("watched", JSON.stringify([...watched, movie]));
   }
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
   useEffect(() => {
     const controller = new AbortController();
     async function fetchMovies() {
